5c94b527a201ca42be82a5e2eb376e584ed87ac4
vanh2001 <anhviet1709@gmail.com>
2022-03-01 16:49:54 +0700

    Add bai copy 7 file

---
+++ CopyBigFile/src/Main.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your source file: ");
+        String sourcePath = scanner.nextLine();
+        System.out.print("Enter destination file: ");
+        String destPath = scanner.nextLine();
+
+        File sourceFile = new File(sourcePath);
+        File destFile = new File(destPath);
+        try {
+            copyFileUsingJava7Files(sourceFile,destFile);
+//            copyFileUsingStream(sourceFile,destFile);
+        }catch (IOException e){
+            System.out.print("Can't copy that file");
+            System.out.print(e.getMessage());
+        }
+    }
+
+    private static void copyFileUsingJava7Files(File source, File dest) throws IOException {
+        Files.copy(source.toPath(),dest.toPath(), StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    private static void copyFileUsingStream(File source, File dest) throws IOException{
+        InputStream is = null;
+        OutputStream os = null;
+        try {
+            is = new FileInputStream(source);
+            os = new FileOutputStream(dest);
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = is.read(buffer))>0){
+                os.write(buffer,0,length);
+            }
+        }finally {
+            is.close();
+            os.close();
+        }
+    }
+}
